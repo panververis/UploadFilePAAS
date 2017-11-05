32921edd651e1d40c40e6e7263131d3f960a86c8
panververis <panververis@gmail.com>
2017-11-05 18:57:46 +0000

    Added dead simple "Upload an image and display it" functionality

--- SampleUploadFile/Controllers/HomeController.cs
+++ SampleUploadFile/Controllers/HomeController.cs
@@ -1,25 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace SampleUploadFile.Controllers {
     public class HomeController : Controller {
+
+        //  "Index" Action
         public ActionResult Index() {
             return View();
         }
 
-        public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
+        //  "Upload a file" action
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase File) {
+            try {
 
-            return View();
-        }
+                // Checking whether a valid file of a size > 0 was selected
+                if (File.ContentLength > 0) {
 
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
+                    //  Getting the Filename
+                    string _FileName = "image.jpg"; //Path.GetFileName(File.FileName);
 
-            return View();
+                    //  Combining the to-be-uploaded file's name with the pre-specified Server - side "Images" folder
+                    string _path = Path.Combine(Server.MapPath("~/Content/Images"), _FileName);
+
+                    //  Lastly, attempt to save (upload) the selected file
+                    File.SaveAs(_path);
+                }
+
+                //  Inform the user of the succesfully uploaded file
+                ViewBag.Message = "File Uploaded Successfully!!";
+
+                //  And return to the home / landing page
+                return RedirectToAction("Index", "Home");
+            }
+            catch {
+
+                //  In case something goes wrong, inform the user that the uploading operation failed
+                ViewBag.Message = "File upload failed!!";
+
+                //  And return to the home / landing page
+                return RedirectToAction("Index", "Home");
+            }
         }
     }
 }
