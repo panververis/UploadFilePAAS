809525b309e410ee865feb3c302a429453a1de46
panververis <panververis@gmail.com>
2017-11-19 15:11:09 +0000

    Added a ViewModel

---
+++ SampleUploadFile/Models/SampleUploadFileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleUploadFile.Models {
+
+    public class SampleUploadFileViewModel {
+        //      Properties
+        public string           SelectedFileName        { get; set; }
+        public string           Message                 { get; set; }
+        public List<string>     UploadedBlobNamesList   { get; set; }
+
+        //      Ctor
+        public SampleUploadFileViewModel() {
+            UploadedBlobNamesList = new List<string>();
+        }
+    }
+
+}
